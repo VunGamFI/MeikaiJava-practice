9473fa3a3d1bf81e9d0fbdbb5e3a02747a04d676
糊掉的麵 <jaqueswin@gmail.com>
2023-08-06 17:08:34 +0800

    Update carTest.java

--- chapter 12/practice12_1/carTest.java
+++ chapter 12/practice12_1/carTest.java
@@ -1,7 +1,7 @@
 package practice12_1;
 
 import practice12_1.CarSub;
-class carTest{
+public class carTest{
     public static void main(String[] args){
         Day d = new Day(2010, 10, 15);
         CarSub myCar = new CarSub("爱车", 1885, 1490, 5220, 90.0, d, 0);
