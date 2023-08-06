8b2e425400a6044d6cfa4edde9ee9537aa880df5
糊掉的麵 <jaqueswin@gmail.com>
2023-08-06 17:09:25 +0800

    Update DateIdTester.java

--- chapter 11/practice11_1/DateIdTester.java
+++ chapter 11/practice11_1/DateIdTester.java
@@ -1,6 +1,6 @@
 package practice11_1;
 import practice11_1.id.DateId;
-class DateIdTester{
+public class DateIdTester{
     public static void main(String[] args){
         DateId a = new DateId();
         DateId b = new DateId();
