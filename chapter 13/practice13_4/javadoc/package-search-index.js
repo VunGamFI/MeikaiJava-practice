7cdbfdd7ca6c3c3ab9647f2aded13f1b8a010c69
糊掉的麵 <jaqueswin@gmail.com>
2023-08-08 21:15:01 +0800

    Add files via upload

---
+++ chapter 13/practice13_4/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"practice13_4"},{"l":"所有程序包","u":"allpackages-index.html"}];updateSearchResults();
---
+++ chapter 13/practice13_4/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"practice13_4","l":"Human"},{"l":"所有类和接口","u":"allclasses-index.html"}];updateSearchResults();
