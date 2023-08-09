02a44ec9af19b9213fdfc444491940e0f04d441e
糊掉的麵 <jaqueswin@gmail.com>
2023-08-09 23:55:37 +0800

    Add files via upload

---
+++ chapter 15/Computer.java
@@ -0,0 +1,17 @@
+package practice15_5;
+
+import java.util.Random;
+public class Computer extends Player {
+    Random rand = new Random();
+
+    private int hand;
+
+    public Computer( ) {
+        hand = rand.nextInt(3);
+        setHand(hand);
+    }
+
+    public String toString(){
+        return "机器人的手势：";
+    }
+}
