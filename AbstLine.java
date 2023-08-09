7817884ba462ed2f8eaee244f02506c67a495e49
糊掉的麵 <jaqueswin@gmail.com>
2023-08-09 17:29:26 +0800

    Add files via upload

---
+++ AbstLine.java
@@ -0,0 +1,21 @@
+package practice14_1;
+
+public abstract class AbstLine extends Shape{
+    private int length;
+
+    public AbstLine(int length){
+        setLength(length);
+    }
+
+    public void setLength(int length){
+        this.length = length;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public String tostring(){
+        return "AbstLine(length: " + length + ")";
+    }
+}
